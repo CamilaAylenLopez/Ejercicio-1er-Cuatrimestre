08e63440673fced38c512125ac60c39aa8e75eef
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-07-03 08:48:24 -0300

    a

--- my-app/src/componentes/Aplicacion.jsx
+++ my-app/src/componentes/Aplicacion.jsx
@@ -5,10 +5,23 @@ const Aplicacion = () => {
     /*const [variable, setVariable] = useState(*valorInicial*)*/
     const [texto, setTexto] = useState("")
     const [contador, setContador] = useState(0)
+    const [letras, setletras] = useState(0)
+    const [input, setInput] = useState("")
+
 
     /**cuando cambia el valor del input se ejecuta lo de adentro */
     const handleChange = (e) =>{
         setTexto(e.target.value)
+        setletras(e.target.value.length)
+    }
+
+    const subirValor = () => {
+        setInput(document.getElementById(1).value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        subirValor();
+        document.getElementById(1).value = ""
     }
 
     /**cuando variable cambie su valor, se ejecutara lo que esta dentro del useEffect */
@@ -21,6 +34,14 @@ const Aplicacion = () => {
             <input onChange={handleChange} />
             <p>Texto: {texto}</p>
             <p>Contador: {contador}</p>
+            <p>Letras: {letras}</p>
+
+            <form onSubmit={handleSubmit}>
+                <input type="text" className="u-full-width" placeholder='texto' id='1'/>
+                <button type="submit" className="u-full-width button-primary">Submit</button>
+            </form>
+
+            <p>{input}</p>
         </>
     )
 }
